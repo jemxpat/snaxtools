6aca0cbf411a609830f66683edf4f8e4da3851b6
jemxpat <jemxpat@gmail.com>
2019-06-23 13:26:42 +0900

    Minor code cleanup

--- voteall.sh
+++ voteall.sh
@@ -16,9 +16,9 @@ usage() {
 }
 
 checkopts() {
-    if [ -x  /usr/local/bin/clisnax.sh ] ; then
+    if [ -x /usr/local/bin/clisnax.sh ]; then
         CLEOS="/usr/local/bin/clisnax.sh"
-    elif [ -x  /usr/local/bin/clisnax ] ; then
+    elif [ -x /usr/local/bin/clisnax ]; then
         # Switch the order of these, if local API exists and is reliable.
         APINODE="--url https://cdn.snax.one"
         APINODE="--url http://127.0.0.1:8888"
@@ -32,23 +32,22 @@ checkopts() {
     fi
     while getopts ":Rk:w:s:p:" opt; do
         case "$opt" in
-            p)  PROD="$OPTARG" ;;
-            w)  WALLET_NAME="$OPTARG" ;;
-            s)  WALLET_FILE="$OPTARG" ;;
-            k)  THIS_KEY="$OPTARG" ;;
-            R)  FORREAL="1" ;;
-            *)  usage && echo "Unrecognized option $OPTARG" && die ;;
+        p) PROD="$OPTARG" ;;
+        w) WALLET_NAME="$OPTARG" ;;
+        s) WALLET_FILE="$OPTARG" ;;
+        k) THIS_KEY="$OPTARG" ;;
+        R) FORREAL="1" ;;
+        *) usage && echo "Unrecognized option $OPTARG" && die ;;
         esac
     done
 }
 
-main()
-{
+main() {
     checkopts $@
     checklock
     make_vote_list
     do_vote
-    $CLEOS wallet lock_all > /dev/null
+    $CLEOS wallet lock_all >/dev/null
     exit 0
 }
 
@@ -64,11 +63,11 @@ make_vote_list() {
     # Active only, unreged producer will cause vote fail.
     make_active_producer_list
     make_ignore_list
-    if [[ "$CHAINPRODLIST" = "" ]] ; then
+    if [[ $CHAINPRODLIST == "" ]]; then
         die "Function ${FUNCNAME[0]}: Failed to get producer list for voting"
     fi
     # We run through the list of BPs and filter out those we do not want.
-    for CheckProd in ${CHAINPRODLIST[@]} ; do
+    for CheckProd in ${CHAINPRODLIST[@]}; do
         # IgnoreBP list
         [[ ${IGNOREBP[$CheckProd]} ]] && continue
         # Adding producer $CheckProd
@@ -77,9 +76,8 @@ make_vote_list() {
         let $((counter++))
         [[ $counter -ge 30 ]] && break
     done
-    [[ "$VLIST" = "" ]] && die "Function ${FUNCNAME[0]}: Failed to get any producer to vote for"
+    [[ $VLIST == "" ]] && die "Function ${FUNCNAME[0]}: Failed to get any producer to vote for"
 }
-
 
 make_ignore_list() {
     declare -Ag IGNOREBP
@@ -91,7 +89,7 @@ make_ignore_list() {
         extranode1 \
         extranode2 \
     "
-    for ignorebp in ${IGNORELIST[@]} ; do
+    for ignorebp in ${IGNORELIST[@]}; do
         IGNOREBP[$ignorebp]=true
     done
 }
@@ -101,27 +99,26 @@ make_active_producer_list() {
     $CLEOS wallet list &>/dev/null
     # Get up to 100 top producers, removing unreged producers
     CHAINPRODLIST=$($CLEOS system listproducers -l 101 | grep -E "^[a-z1-5]" | grep -v 111111111111 | sort | awk '{print $1}')
-    if [[ "$CHAINPRODLIST" = "" ]] ; then
+    if [[ $CHAINPRODLIST == "" ]]; then
         die "Function ${FUNCNAME[0]}: Failed to get producer list"
     fi
 }
-
 
 unlock_wallet() {
     local unlock_wallet=$1
     local unlock_file=$2
     check_open_wallet $unlock_wallet
-    if [ ! -r $unlock_file ] ; then
+    if [ ! -r $unlock_file ]; then
         die "Function ${FUNCNAME[0]}: Cannot read wallet pw file $unlock_file"
     fi
-    if [ ! $OPENWALLET ] ; then
-        $CLEOS wallet unlock -n $unlock_wallet --password $(cat $unlock_file) > /dev/null
+    if [ ! $OPENWALLET ]; then
+        $CLEOS wallet unlock -n $unlock_wallet --password $(cat $unlock_file) >/dev/null
     else
         return
     fi
     # Now, make sure it is open, or die
     check_open_wallet $unlock_wallet
-    if [ ! $OPENWALLET ] ; then
+    if [ ! $OPENWALLET ]; then
         die "Function ${FUNCNAME[0]}: Failed to unlock wallet $unlock_wallet"
     fi
 }
@@ -136,28 +133,34 @@ check_open_wallet() {
     # return OPENWALLET
 }
 
-die() { echo "$@" 1>&2 ; exit 1; }
+die() {
+    echo "$@" 1>&2
+    exit 1
+}
 
 PROGNAME=${0##*/}
 LOCKFILE="/var/lock/$PROGNAME"
 LOCKFD=98
 
 # PRIVATE
-_lock()             { flock -$1 $LOCKFD; }
-_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
-_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+_lock() { flock -$1 $LOCKFD; }
+_no_more_locking() {
+    _lock u
+    _lock xn && rm -f $LOCKFILE
+}
+_prepare_locking() {
+    eval "exec $LOCKFD>\"$LOCKFILE\""
+    trap _no_more_locking EXIT
+}
 # PUBLIC
-exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
-exlock()            { _lock x; }   # obtain an exclusive lock
-shlock()            { _lock s; }   # obtain a shared lock
-unlock()            { _lock u; }   # drop a lock
+exlock_now() { _lock xn; } # obtain an exclusive lock immediately or fail
+exlock() { _lock x; }      # obtain an exclusive lock
+shlock() { _lock s; }      # obtain a shared lock
+unlock() { _lock u; }      # drop a lock
 checklock() {
-    _prepare_locking # ON START
+    _prepare_locking                              # ON START
     exlock_now || die "$PROGNAME already running" # Simplest example is avoiding running multiple instances of script.
 }
 
-
-
 main $@
 exit 0
-
